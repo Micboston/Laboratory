5984713b45faeb356f36115606f28d11150d2371
Micboston <61673031+Micboston@users.noreply.github.com>
2020-08-20 23:12:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ function set(mode) {
    mouth1Border = "black";
    mouth1Color = "white";
    anima = animaa;
-   celebration.play();
+  enableLoop();
   } else {
    focus = "You passed the point";
    color = "black";
@@ -142,7 +142,7 @@ function set(mode) {
    mouth1Border = "black";
    mouth1Color = "white";
    anima = animaa;
-   celebration.play();
+   enableLoop();
   } else {
    focus = "You passed the point";
    color = "black";
